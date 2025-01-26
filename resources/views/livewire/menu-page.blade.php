b84f555e61888c136c3c148215a0e87d856d596c
Angelina Joyvina <angelinajoyvina@gmail.com>
2025-01-26 21:00:55 +0700

    add banner in menu page

--- resources/views/livewire/menu-page.blade.php
+++ resources/views/livewire/menu-page.blade.php
@@ -1,3 +1,17 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <div class="w-full h-[70vh] bg-[#FDF6E1] relative">
+        <img class="relative ml-[15vw]" src="{{ asset('images/menu-tropical-sunny.svg') }}" alt="Menu Tropical Sunny">
+        <img class="ml-[50vw] mt-[-53vh] relative" src="{{ asset('images/menu-best-seller.svg') }}" alt="Menu Best Seller">
+        <img class="ml-[50vw] mt-[5vh] relative" src="{{ asset('images/menu-tropical-sunny-splash.svg') }}"
+            alt="Menu Tropical Sunny Splash">
+        <img class="ml-[50vw] mt-[5vh] relative" src="{{ asset('images/menu-tropical-desc.svg') }}"
+            alt="Menu Tropical Description">
+        <a wire:navigate href="/menu">
+            <p
+                class="bg-[#6FE20D] text-white font-afacad text-[1.2vw] w-[10vw] h-[7vh] relative mt-[3vh] flex items-center justify-center font-bold rounded-[0.5vw] ml-[50vw]">
+                Order Now
+            </p>
+        </a>
+    </div>
+
 </div>
