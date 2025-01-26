d8fe3864f3d3a212fff7b1c8003b06bc56ecef5d
Richard <richard123yes@gmail.com>
2025-01-26 21:10:36 +0700

    Create Best Seller information in Menu Page

--- resources/views/livewire/menu-page.blade.php
+++ resources/views/livewire/menu-page.blade.php
@@ -13,5 +13,22 @@
             </p>
         </a>
     </div>
+    
+    <div class="w-full h-[3vh] bg-white relative">
+    </div>
 
+    <div class="w-full h-[70vh] bg-[#FFF1EB] relative">
+        <img class="relative ml-[8vw]" src="{{ asset('images/menu-berry-banana.svg') }}" alt="Menu Berry Banana">
+        <img class="relative ml-[60vw] mt-[-70vh]" src="{{ asset('images/menu-citrus-berry.svg') }}"
+            alt="Menu Citrus Berry">
+        <img class="relative ml-[37vw] mt-[-20vh]" src="{{ asset('images/menu-best-duo.svg') }}" alt="Menu Best Duo">
+        <div class="w-[20vw] h-[8vh] relative ml-[39vw] mt-[15vh]">
+            <img src="{{ asset('images/menu-berry-banana-bliss.svg') }}" alt="Menu Berry Banana Bliss">
+            <p class="font-afacad text-[1.5vw] relative">Strawberry + Banana</p>
+        </div>
+        <div class="w-[20vw] h-[8vh] relative ml-[70vw] mt-[-10vh]">
+            <img src="{{ asset('images/menu-citrus-berry-zing.svg') }}" alt="Menu Citrus Berry Zing">
+            <p class="font-afacad text-[1.5vw] relative">Orange + Strawberry</p>
+        </div>
+    </div>
 </div>
