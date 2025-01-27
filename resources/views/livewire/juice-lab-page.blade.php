ee95d4bf1aeecd186e496d029ade2e19809fcaea
Angelina Joyvina <angelinajoyvina@gmail.com>
2025-01-27 09:41:52 +0700

    Create Banner in JuiceLab

--- resources/views/livewire/juice-lab-page.blade.php
+++ resources/views/livewire/juice-lab-page.blade.php
@@ -1,4 +1,16 @@
 <div>
+    <div class="relative h-[60vh]">
+        <img class="w-full mt-[-13vh]" src="{{ asset('images/juicelab-bg.svg') }}" alt="JuiceLab Background">
+        <img class="relative mt-[-50vh] ml-[24vw]" src="{{ asset('images/juicelab-hiasan-jeruk.svg') }}"
+            alt="JuiceLab Hiasan Jeruk">
+        <img class="relative mt-[-95vh] ml-[56vw]" src="{{ asset('images/juicelab-hiasan-botol.svg') }}"
+            alt="JuiceLab Hiasan Botol">
+        <img class="relative mt-[-56vh] ml-[9vw]" src="{{ asset('images/juicelab-title.svg') }}" alt="JuiceLab Title">
+        <img class="relative ml-[9vw] mt-[3vh]" src="{{ asset('images/juicelab-description.svg') }}"
+            alt="JuiceLab Description">
+        <img class="relative ml-[9vw] mt-[5vh]" src="{{ asset('images/juicelab-try-it.svg') }}" alt="JuiceLab Try It">
+    </div>
+    
     <div class="bg-[#FDF6E1] relative w-full h-[80vh] z-[-10] mt-[20vh] pt-[15vh]">
         <div class="relative w-[30vw] h-[10vh] ml-[38vw] flex flex-row">
             <p class="font-afacad text-[5vw] font-bold text-[#000000]">How It </p>
