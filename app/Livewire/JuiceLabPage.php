0b36829cf36270a1252323fb7123a32c89484c54
Kevin Tan <kvintan26@gmail.com>
2025-01-27 09:02:53 +0700

    Add How it Works in Juice Lab Page

---
+++ app/Livewire/JuiceLabPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class JuiceLabPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.juice-lab-page');
+    }
+}
---
+++ resources/views/livewire/juice-lab-page.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div class="bg-[#FDF6E1] relative w-full h-[80vh] z-[-10] mt-[20vh] pt-[15vh]">
+        <div class="relative w-[30vw] h-[10vh] ml-[38vw] flex flex-row">
+            <p class="font-afacad text-[5vw] font-bold text-[#000000]">How It </p>
+            <p class="font-afacad text-[5vw] font-bold text-[#FD7034] ml-[0.5vw]">Works</p>
+        </div>
+
+        <div class="relative w-[80vw] h-[40vh] flex flex-row gap-[5vw] mt-[5vh] ml-[10vw]">
+            <img src="{{ asset('images/juicelab-howitworks-3-tips.svg') }}" alt="JuiceLab How It Works 3 Steps">
+        </div>
+
+    </div>
+</div>
