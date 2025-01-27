7555d953a3d5b0c80597ceadb3043bc107ec5b48
Kevin Tan <kvintan26@gmail.com>
2025-01-27 21:56:45 +0700

    Create Banner in Contact Us Page

---
+++ resources/views/livewire/contact-us-page.blade.php
@@ -0,0 +1,30 @@
+<div>
+    <div class="h-[110vh] relative justify-items-center z-[-10]">
+        <img class="relative z-[-10] mt-[-13vh] h-auto w-[100vw]" src="{{ asset('images/contact-us-background.svg') }}"
+            alt="Contact Us Background">
+        <img class="mt-[-50vh]" src="{{ asset('images/contact-us-tagline.svg') }}" alt="Contact Us Tagline">
+        <p class="font-afacad font-semibold text-white text-wrap text-[1.5vw] w-[37vw] text-center">Create your own
+            fresh and healthy
+            juice
+            that
+            tastes
+            delicious.
+        <p class="font-afacad font-semibold text-white text-wrap text-[1.5vw] w-[37vw] text-center"> We use the best
+            quality ingredients.
+        </p>
+        <p class="font-afacad font-semibold text-white text-wrap text-[1.5vw] w-[50vw] text-center"> Continuously looking
+            for innovative ways to make healthy living easy.
+        </p>
+        <p class="font-afacad font-semibold text-white text-wrap text-[1.5vw] w-[40vw] text-center"> Always providing
+            very friendly service so that customers have a reason to smile.</p>
+        </p>
+        <img class="mt-[-52vh] ml-[70vw]" src="{{ asset('images/contact-us-hiasan-kanan.svg') }}"
+            alt="Contact Us Hiasan Kanan">
+        <img class="mt-[-60vh] ml-[-70vw]" src="{{ asset('images/contact-us-hiasan-kiri.svg') }}"
+            alt="Contact Us Hiasan Kiri">
+        <p class="font-afacad w-[18vw] text-wrap text-center text-[1.4vw] mt-[10vh]">Got something on your mind?
+            We'd love to hear from you!</p>
+        <p class="font-bold font-afacad text-[2vw] mt-[3vh]">Contact Us :</p>
+    </div>
+
+</div>
