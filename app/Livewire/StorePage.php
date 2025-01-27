db659475bb804121d2822fe33e29fba53cc424d2
Angelina Joyvina <angelinajoyvina@gmail.com>
2025-01-27 22:07:02 +0700

    Create Store Location in Store Page

---
+++ app/Livewire/StorePage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class StorePage extends Component
+{
+    public function render()
+    {
+        return view('livewire.store-page');
+    }
+}
