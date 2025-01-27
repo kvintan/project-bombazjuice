c20bf5749fe5cf4b67f27d9b2e3fe5e4276644a5
Shanella <nenelchristina@gmail.com>
2025-01-27 22:12:28 +0700

    Create List of Fruits to Mix

--- resources/views/livewire/juice-lab-page.blade.php
+++ resources/views/livewire/juice-lab-page.blade.php
@@ -22,4 +22,56 @@
         </div>
 
     </div>
+
+    <div class="relative w-full h-[160vh]">
+        <div class="relative w-[30vw] h-[10vh] ml-[40vw] mt-[5vh] flex flex-row">
+            <p class="font-afacad text-[5vw] font-bold text-[#000000]">Let's</p>
+            <p class="font-afacad text-[5vw] font-bold text-[#F9B401] ml-[1vw]">Start</p>
+        </div>
+
+
+        <div class="relative w-[80vw] h-[40vh] mt-[10vh] ml-[10vw] flex flex-row gap-[7vw]">
+            <img src="{{ asset('images/juicelab-start-one.svg') }}" alt="JuiceLab Start One">
+            <img src="{{ asset('images/juicelab-start-two.svg') }}" alt="JuiceLab Start Two">
+            <img src="{{ asset('images/juicelab-start-three.svg') }}" alt="JuiceLab Start Three">
+        </div>
+
+        <p class="relative text-black text-[5vw] ml-[30vw] mt-[10vh] font-afacad font-bold">Choose 3 kind of fruit</p>
+
+        <div class="relative w-[76vw] h-[60vh] mt-[10vh] ml-[12vw] flex flex-col gap-[5vh]">
+            <div class="relative flex flex-row gap-[3.1vw]">
+                <img src="{{ asset('images/juicelab-buah-1.svg') }}" alt="JuiceLab Buah 1">
+                <img src="{{ asset('images/juicelab-buah-2.svg') }}" alt="JuiceLab Buah 2">
+                <img src="{{ asset('images/juicelab-buah-3.svg') }}" alt="JuiceLab Buah 3">
+                <img src="{{ asset('images/juicelab-buah-4.svg') }}" alt="JuiceLab Buah 4">
+                <img src="{{ asset('images/juicelab-buah-5.svg') }}" alt="JuiceLab Buah 5">
+                <img src="{{ asset('images/juicelab-buah-6.svg') }}" alt="JuiceLab Buah 6">
+            </div>
+
+
+            <div class="relative flex flex-row gap-[3.1vw]">
+                <img src="{{ asset('images/juicelab-buah-7.svg') }}" alt="JuiceLab Buah 7">
+                <img src="{{ asset('images/juicelab-buah-8.svg') }}" alt="JuiceLab Buah 8">
+                <img src="{{ asset('images/juicelab-buah-9.svg') }}" alt="JuiceLab Buah 9">
+                <img src="{{ asset('images/juicelab-buah-10.svg') }}" alt="JuiceLab Buah 10">
+                <img src="{{ asset('images/juicelab-buah-11.svg') }}" alt="JuiceLab Buah 11">
+                <img src="{{ asset('images/juicelab-buah-12.svg') }}" alt="JuiceLab Buah 12">
+            </div>
+
+
+            <div class="relative flex flex-row text-center">
+                <div
+                    class="font-bold relative font-afacad text-white text-[2vw] rounded-[2vw] h-[5vh] w-[10vw] bg-[#FF7676]">
+                    Clear
+                </div>
+
+                <div
+                    class="font-bold ml-[60vw] relative font-afacad text-white text-[2vw] rounded-[2vw] h-[5vh] w-[10vw] bg-[#9DD071]">
+                    Done
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 </div>
