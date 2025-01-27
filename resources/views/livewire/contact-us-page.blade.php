e08224e89e1b473a38e538c13cc4a7eebe2a814a
Richard <richard123yes@gmail.com>
2025-01-27 21:59:27 +0700

    Information Contact in Contact Us Page

--- resources/views/livewire/contact-us-page.blade.php
+++ resources/views/livewire/contact-us-page.blade.php
@@ -25,6 +25,28 @@
         <p class="font-afacad w-[18vw] text-wrap text-center text-[1.4vw] mt-[10vh]">Got something on your mind?
             We'd love to hear from you!</p>
         <p class="font-bold font-afacad text-[2vw] mt-[3vh]">Contact Us :</p>
+        <div class="w-[35vw] h-[20vh] mt-[2vh]">
+            <div class="w-[20vw] h-[8vh]">
+                <img src="{{ asset('images/contact-us-phone.svg') }}" alt="Contact Us Phone">
+                <p class="font-afacad font-bold text-[1.5vw] ml-[3.5vw] mt-[-5.5vh]">Phone Number</p>
+                <p class="font-afacad text-[1.3vw] ml-[3.5vw] mt-[-1vh]">089134523621456</p>
+            </div>
+            <div class="w-[20vw] h-[8vh] mt-[4vh]">
+                <img src="{{ asset('images/contact-us-email.svg') }}" alt="Contact Us Email">
+                <p class="font-afacad font-bold text-[1.5vw] ml-[3.5vw] mt-[-5.5vh]">Email Address</p>
+                <p class="font-afacad text-[1.3vw] ml-[3.5vw] mt-[-1vh]">BomBazjuice@gmail.com</p>
+            </div>
+            <div class="w-[20vw] h-[8vh] ml-[23vw] mt-[-20vh]">
+                <img src="{{ asset('images/contact-us-facebook.svg') }}" alt="Contact Us Facebook">
+                <p class="font-afacad font-bold text-[1.5vw] ml-[3.5vw] mt-[-5.5vh]">Facebook</p>
+                <p class="font-afacad text-[1.3vw] ml-[3.5vw] mt-[-1vh]">@BombazJuice</p>
+            </div>
+            <div class="w-[20vw] h-[8vh] ml-[23vw] mt-[4vh]">
+                <img src="{{ asset('images/contact-us-instagram.svg') }}" alt="Contact Us Instagram">
+                <p class="font-afacad font-bold text-[1.5vw] ml-[3.5vw] mt-[-5.5vh]">Instagram</p>
+                <p class="font-afacad text-[1.3vw] ml-[3.5vw] mt-[-1vh]">@BombazJuice</p>
+            </div>
+        </div>
     </div>
 
 </div>
