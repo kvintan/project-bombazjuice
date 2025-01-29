889484b625f8752c36c68facdd923ad44d1b247c
Kevin Tan <kvintan26@gmail.com>
2025-01-29 20:52:02 +0700

    Make Users Database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('has_logged_in')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
