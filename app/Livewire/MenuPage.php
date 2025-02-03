a95029505c77591030b5e30bef282865fc1a21f6
ijlfr <rifzikaijlal12@gmail.com>
2025-02-03 16:30:54 +0700

    Create Show Order Menu List

--- app/Livewire/MenuPage.php
+++ app/Livewire/MenuPage.php
@@ -2,12 +2,50 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
+use App\Models\Product;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use Livewire\WithPagination;
 
+#[Title('Menu - BomBazJuice')]
 class MenuPage extends Component
 {
+    use LivewireAlert;
+    
+    use WithPagination;
+
+    public function addToCart($product_id) {
+        $total_count = CartManagement::addItemToCart($product_id);
+
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        $this->alert('success', 'Product added to the cart successfully!', [
+            'position' => 'bottom-end',
+            'timer' => 3000,
+            'toats' => true,
+        ]);
+    }
+
     public function render()
     {
-        return view('livewire.menu-page');
+        $allProducts = Product::query()
+            ->where('is_active', 1)
+            ->get();
+
+        $first12 = $allProducts->slice(0, 12);
+        $next8 = $allProducts->slice(12, 8);
+
+        $paginatedProducts = Product::query()
+            ->where('is_active', 1)
+            ->paginate(20);
+
+        return view('livewire.menu-page', [
+            'products' => $paginatedProducts,
+            'first12' => $first12,
+            'next8' => $next8,
+        ]);
     }
 }
