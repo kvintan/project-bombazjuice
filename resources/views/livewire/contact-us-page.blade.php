6dbf13f04c7e4d91e80e7cd46cbb01a82df1730c
ijlfr <rifzikaijlal12@gmail.com>
2025-03-05 12:55:31 +0700

    Create responsive tablet and mobile for Contact Us

--- resources/views/livewire/contact-us-page.blade.php
+++ resources/views/livewire/contact-us-page.blade.php
@@ -1,50 +1,88 @@
 <div>
-    <div class="h-[110vh] relative justify-items-center z-[-10]">
-        <img class="relative z-[-10] mt-[-13vh] h-auto w-[100vw]" src="{{ asset('images/contact-us-background.svg') }}"
-            alt="Contact Us Background">
-        <img class="mt-[-50vh]" src="{{ asset('images/contact-us-tagline.svg') }}" alt="Contact Us Tagline">
-        <p class="font-afacad font-semibold text-white text-wrap text-[1.5vw] w-[37vw] text-center">Create your own
+    <div class="h-[110vh] sm:h-[80vh] lg:h-[120vh] relative justify-items-center z-[-10]">
+        <img class="relative z-[-10] invisible sm:invisible lg:scale-[100%] lg:visible lg:mt-[-13vh] h-auto w-[100vw]"
+            src="{{ asset('images/contact-us-background.svg') }}" alt="Contact Us Background">
+        <img class="sm:invisible lg:invisible relative w-[100%] mt-[-30vh]"
+            src="{{ asset('images/contact-us-mobile-background.svg') }}" alt="Contact Us Mobile Background">
+        <img class="invisible sm:visible relative mt-[-90vh] lg:invisible"
+            src="{{ asset('images/contact-us-tablet-background.svg') }}" alt="Contact Us Tablet Background">
+        <img class="mt-[28vh] scale-[80%] sm:mt-[-25vh] relative lg:scale-[100%] lg:mt-[-210vh]"
+            src="{{ asset('images/contact-us-tagline.svg') }}" alt="Contact Us Tagline">
+        <p
+            class="font-afacad font-semibold text-white text-wrap text-[3vw] w-[70vw] relative sm:text-[1.5vw] lg:mt-[0vh] lg:text-[1.5vw] lg:w-[37vw] text-center">
+            Create your
+            own
             fresh and healthy
             juice
             that
             tastes
             delicious.
-        <p class="font-afacad font-semibold text-white text-wrap text-[1.5vw] w-[37vw] text-center"> We use the best
+        <p
+            class="font-afacad font-semibold text-white text-wrap relative text-[3vw] w-[70vw] sm:text-[1.5vw]  lg:mt-[0vh] lg:text-[1.5vw] lg:w-[37vw] text-center">
+            We use the
+            best
             quality ingredients.
         </p>
-        <p class="font-afacad font-semibold text-white text-wrap text-[1.5vw] w-[50vw] text-center"> Continuously looking
+        <p
+            class="font-afacad font-semibold text-white text-wrap relative text-[3vw] w-[70vw] sm:text-[1.5vw] lg:mt-[0vh] lg:text-[1.5vw] lg:w-[50vw] text-center">
+            Continuously
+            looking
             for innovative ways to make healthy living easy.
         </p>
-        <p class="font-afacad font-semibold text-white text-wrap text-[1.5vw] w-[40vw] text-center"> Always providing
+        <p
+            class="font-afacad font-semibold text-white text-wrap relative text-[3vw] w-[70vw] sm:text-[1.5vw] lg:text-[1.5vw] lg:w-[40vw] text-center">
+            Always
+            providing
             very friendly service so that customers have a reason to smile.</p>
         </p>
-        <img class="mt-[-52vh] ml-[70vw]" src="{{ asset('images/contact-us-hiasan-kanan.svg') }}"
-            alt="Contact Us Hiasan Kanan">
-        <img class="mt-[-60vh] ml-[-70vw]" src="{{ asset('images/contact-us-hiasan-kiri.svg') }}"
-            alt="Contact Us Hiasan Kiri">
-        <p class="font-afacad w-[18vw] text-wrap text-center text-[1.4vw] mt-[10vh]">Got something on your mind?
+        <img class="scale-[45%] ml-[56vw] mt-[-25vh] sm:scale-[50%] sm:ml-[68vw] lg:scale-[90%] lg:mt-[-52vh] lg:ml-[68vw]"
+            src="{{ asset('images/contact-us-hiasan-kanan.svg') }}" alt="Contact Us Hiasan Kanan">
+        <img class="scale-[60%] ml-[-40vw] mt-[-45vh] sm:ml-[-60vw] sm:mt-[-43vh] lg:scale-[100%] lg:mt-[-60vh] lg:ml-[-70vw]"
+            src="{{ asset('images/contact-us-hiasan-kiri.svg') }}" alt="Contact Us Hiasan Kiri">
+        <p class="font-afacad w-[50vw] sm:text-[2vw] lg:w-[18vw] text-wrap text-center lg:text-[1.4vw] lg:mt-[10vh]">Got
+            something on
+            your
+            mind?
             We'd love to hear from you!</p>
-        <p class="font-bold font-afacad text-[2vw] mt-[3vh]">Contact Us :</p>
-        <div class="w-[35vw] h-[20vh] mt-[2vh]">
-            <div class="w-[20vw] h-[8vh]">
-                <img src="{{ asset('images/contact-us-phone.svg') }}" alt="Contact Us Phone">
-                <p class="font-afacad font-bold text-[1.5vw] ml-[3.5vw] mt-[-5.5vh]">Phone Number</p>
-                <p class="font-afacad text-[1.3vw] ml-[3.5vw] mt-[-1vh]">089134523621456</p>
+        <p class="font-bold font-afacad mt-[2vh] text-[6vw] sm:text-[4vw] lg:text-[2vw] lg:mt-[3vh]">Contact Us :</p>
+        <div class="w-[35vw] mt-[2vh] lg:mt-[2vh]">
+            <div class="w-[30vw] h-[10vh] lg:w-[20vw] lg:h-[8vh]">
+                <img class="ml-[-15vw] sm:ml-[-11vw] lg:ml-[0vw]" src="{{ asset('images/contact-us-phone.svg') }}"
+                    alt="Contact Us Phone">
+                <p
+                    class="font-afacad font-bold text-[3vw] ml-[-5vw] mt-[-4vh] lg:text-[1.5vw] lg:ml-[3.5vw] lg:mt-[-5.5vh]">
+                    Phone
+                    Number</p>
+                <p class="font-afacad text-[3vw] ml-[-5vw] mt-[-0.5vh] lg:text-[1.3vw] lg:ml-[3.5vw] lg:mt-[-1vh]">
+                    089134523621456
+                </p>
             </div>
-            <div class="w-[20vw] h-[8vh] mt-[4vh]">
-                <img src="{{ asset('images/contact-us-email.svg') }}" alt="Contact Us Email">
-                <p class="font-afacad font-bold text-[1.5vw] ml-[3.5vw] mt-[-5.5vh]">Email Address</p>
-                <p class="font-afacad text-[1.3vw] ml-[3.5vw] mt-[-1vh]">BomBazjuice@gmail.com</p>
+            <div class="w-[30vw] h-[10vh] lg:w-[20vw] lg:h-[8vh] lg:mt-[4vh]">
+                <img class="ml-[-15vw] sm:ml-[-11vw] lg:ml-[0vw]" src="{{ asset('images/contact-us-email.svg') }}"
+                    alt="Contact Us Email">
+                <p
+                    class="font-afacad font-bold text-[3vw] ml-[-5vw] mt-[-3.5vh] lg:text-[1.5vw] lg:ml-[3.5vw] lg:mt-[-5.5vh]">
+                    Email Address</p>
+                <p class="font-afacad text-[2.5vw] ml-[-5vw] mt-[-0.4vh] lg:text-[1.3vw] lg:ml-[3.5vw] lg:mt-[-1vh]">
+                    BomBazjuice@gmail.com</p>
             </div>
-            <div class="w-[20vw] h-[8vh] ml-[23vw] mt-[-20vh]">
+            <div class="w-[30vw] h-[10vh] lg:w-[20vw] lg:h-[8vh] ml-[23vw] mt-[-20vh]">
                 <img src="{{ asset('images/contact-us-facebook.svg') }}" alt="Contact Us Facebook">
-                <p class="font-afacad font-bold text-[1.5vw] ml-[3.5vw] mt-[-5.5vh]">Facebook</p>
-                <p class="font-afacad text-[1.3vw] ml-[3.5vw] mt-[-1vh]">@BombazJuice</p>
+                <p
+                    class="font-afacad font-bold text-[3vw] ml-[10vw] mt-[-4vh] sm:ml-[6vw] lg:text-[1.5vw] lg:ml-[3.5vw] lg:mt-[-5.5vh]">
+                    Facebook</p>
+                <p
+                    class="font-afacad text-[3vw] ml-[10vw] mt-[-0.5vh] sm:ml-[6vw] lg:text-[1.3vw] lg:ml-[3.5vw] lg:mt-[-1vh]">
+                    @BombazJuice</p>
             </div>
-            <div class="w-[20vw] h-[8vh] ml-[23vw] mt-[4vh]">
+            <div class="w-[30vw] h-[10vh] lg:w-[20vw] lg:h-[8vh] ml-[23vw] lg:mt-[4vh]">
                 <img src="{{ asset('images/contact-us-instagram.svg') }}" alt="Contact Us Instagram">
-                <p class="font-afacad font-bold text-[1.5vw] ml-[3.5vw] mt-[-5.5vh]">Instagram</p>
-                <p class="font-afacad text-[1.3vw] ml-[3.5vw] mt-[-1vh]">@BombazJuice</p>
+                <p
+                    class="font-afacad font-bold text-[3vw] ml-[10vw] mt-[-4vh] sm:ml-[6vw] lg:text-[1.5vw] lg:ml-[3.5vw] lg:mt-[-5.5vh]">
+                    Instagram</p>
+                <p
+                    class="font-afacad text-[3vw] ml-[10vw] mt-[-0.5vh] sm:ml-[6vw] lg:text-[1.3vw] lg:ml-[3.5vw] lg:mt-[-1vh]">
+                    @BombazJuice</p>
             </div>
         </div>
     </div>
