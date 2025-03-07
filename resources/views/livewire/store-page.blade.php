fd81d1071f08c1caa3304b2a51ab38f5107786c3
Richard <richard123yes@gmail.com>
2025-03-07 21:17:47 +0700

    All responsive store page

--- resources/views/livewire/store-page.blade.php
+++ resources/views/livewire/store-page.blade.php
@@ -4,26 +4,49 @@
             class="relative h-[115vh] mt-[-14vh] z-[-10] justify-center bg-[linear-gradient(176deg,#A5CE30_0%,#BBDA64_20%,#D2E797_40%,#FFFFFF_71%)]">
         </div>
         <div class="justify-items-center relative">
-            <img class="relative mt-[-100vh]" src="{{ asset('images/store-tagline.svg') }}" alt="Store Tagline">
-            <img class="relative" src="{{ asset('images/store-location.svg') }}" alt="Store Location">
-            <div class="bg-FBFBFB-bg mt-[5vh] w-[63vw] h-[63vh] rounded-[1vw] shadow-2xl pt-[3vh] pl-[1vw]">
-                <img src="{{ asset('images/store-pertama.svg') }}" alt="Store Pertama">
-                <img class="ml-[15.5vw] mt-[-17.7vh]" src="{{ asset('images/store-kedua.svg') }}" alt="Store Kedua">
-                <img src="{{ asset('images/store-ketiga.svg') }}" alt="Store Ketiga">
-                <img class="ml-[12.5vw] mt-[-17.7vh]" src="{{ asset('images/store-keempat.svg') }}" alt="Store Keempat">
+            <img class="relative mt-[-100vh] lg:mt-[-100vh]" src="{{ asset('images/store-tagline.svg') }}"
+                alt="Store Tagline">
+            <img class="relative sm:scale-[100%] lg:scale-[100%] scale-[70%]"
+                src="{{ asset('images/store-location.svg') }}" alt="Store Location">
+            <div
+                class="bg-FBFBFB-bg lg:mt-[5vh] lg:w-[63vw] w-[75vw] sm:w-[80vw] h-[35vh] lg:h-[63vh] mt-[2vh] rounded-[1vw] shadow-2xl pt-[3vh] pl-[1vw]">
+                <img class="scale-[30%] mt-[-7.5vh] ml-[36vw] sm:scale-[70%] sm:mt-[-4vh] sm:ml-[-4vw] lg:scale-[100%] lg:mt-[0vh] lg:ml-[0vw]"
+                    src="{{ asset('images/store-pertama.svg') }}" alt="Store Pertama">
+                <img class="scale-[35%] mt-[-10vh] ml-[39.5vw] sm:scale-[70%] sm:mt-[-12.3vh] sm:ml-[16vw] lg:scale-[100%] lg:ml-[15.5vw] lg:mt-[-17.7vh]"
+                    src="{{ asset('images/store-kedua.svg') }}" alt="Store Kedua">
+                <img class="scale-[35%] mt-[-9.6vh] ml-[41vw] sm:scale-[70%] sm:mt-[-3.5vh] sm:ml-[-3vw] lg:scale-[100%] lg:mt-[0vh] lg:ml-[0vw]"
+                    src="{{ asset('images/store-ketiga.svg') }}" alt="Store Ketiga">
+                <img class="scale-[30%] mt-[-10vh] ml-[35vw] sm:scale-[70%] sm:mt-[-12.3vh] sm:ml-[12vw] lg:scale-[100%] lg:ml-[12.5vw] lg:mt-[-17.7vh]"
+                    src="{{ asset('images/store-keempat.svg') }}" alt="Store Keempat">
             </div>
-            <div class="w-[28.5vw] h-[20vh] mt-[-24.5vh] ml-[-31vw]">
-                <p class="font-afacad font-bold text-[2.5vw]">Bogor City</p>
-                <p class="font-arimo text-[1vw] leading-[2vh]">Jl. Pakuan No.3, Sumur Batu, Kec. Babakan Madang,
+            <div class="w-[28.5vw] h-[20vh] lg:mt-[-24.5vh] lg:ml-[-31vw]">
+                <p
+                    class="font-afacad font-bold text-[4vw] mt-[-12.5vh] ml-[-20vw] sm:mt-[-17.5vh] sm:ml-[-23vw] lg:ml-[0vw] lg:mt-[0vh] lg:text-[2.5vw]">
+                    Bogor
+                    City</p>
+                <p
+                    class="font-arimo text-[1.5vw] ml-[-19.5vw] sm:ml-[-22.5vw] w-[40vw] lg:ml-[0vw] lg:mt-[0vh] lg:text-[1vw] leading-[2vh]">
+                    Jl.
+                    Pakuan No.3, Sumur Batu, Kec. Babakan Madang,
                     Kabupaten
                     Bogor, Jawa Barat 16810</p>
-                <p class="font-arimo text-[1vw] mt-[1vh]">Monday - Sunday</p>
-                <p class="font-arimo text-[1vw] mt-[-0.5vh]">Open in 10.00 - 22.00</p>
-                <p class="font-arimo text-[0.8vw] mt-[1vh]">Facilty : Meeting Room, Toilet, .....</p>
+                <p
+                    class="font-arimo text-[1.5vw] ml-[-19.5vw] sm:ml-[-22.5vw] mt-[0.5vh] lg:ml-[0vw] lg:text-[1vw] lg:mt-[1vh]">
+                    Monday
+                    - Sunday
+                </p>
+                <p
+                    class="font-arimo text-[1.5vw] ml-[-19.5vw] sm:ml-[-22.5vw] lg:ml-[0vw] lg:text-[1vw] lg:mt-[-0.5vh]">
+                    Open in 10.00 -
+                    22.00</p>
+                <p
+                    class="font-arimo text-[1.5vw] ml-[-19.5vw] sm:ml-[-22.5vw] mt-[1vh] lg:ml-[0vw] lg:text-[0.8vw] lg:mt-[1vh]">
+                    Facilty :
+                    Meeting Room, Toilet, .....</p>
             </div>
             <a wire:navigate
                 href="https://www.google.com/maps/place/Rumah+Talenta+BCA/@-6.5886849,106.8798058,961m/data=!3m2!1e3!4b1!4m6!3m5!1s0x2e69c7ff4c5bd601:0xbaf218b456b897e4!8m2!3d-6.5886902!4d106.8823807!16s%2Fg%2F11l0dmgc3q?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">
-                <img class="mt-[-54.5vh] ml-[29.5vw] h-[56vh] hover:brightness-[80%]"
+                <img class="h-[26vh] ml-[-10vw] mt-[-43.5vh] sm:ml-[38vw] sm:mt-[-36vh] lg:mt-[-54.5vh] lg:ml-[29.5vw] lg:h-[56vh] hover:brightness-[80%]"
                     src="{{ asset('images/store-map.svg') }}" alt="Store Map">
             </a>
         </div>
