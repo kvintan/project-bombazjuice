f54fa2755ce88ae0448edb67c284c765c5b10a81
Kevin Tan <kvintan26@gmail.com>
2025-03-07 21:24:20 +0700

    Fix Register Page

--- resources/views/livewire/auth/register-page.blade.php
+++ resources/views/livewire/auth/register-page.blade.php
@@ -1,18 +1,20 @@
 <div>
 
-    <div class="h-[105vh] pl-[5vw] relative overflow-hidden">
+    <div class="h-[90vh] sm:h-[120vh] lg:h-[105vh] pl-[5vw] relative overflow-hidden">
 
-        <div class="flex h-full items-center relative z-[10] ml-[-60vw]">
+        <div class="flex h-full items-center relative z-[10] mt-[20vh] sm:ml-[-30vw] lg:mt-[0vh] ml-[0vw] lg:ml-[-60vw]">
             <main class="w-full max-w-md mx-auto p-6 relative">
                 <div
-                    class="w-[40vw] h-[95vh] relative pt-[5vh] pl-[5vw] pr-[5vw] rounded-[2vw] border-[0.3vw] shadow-inset-register">
+                    class="lg:w-[40vw] lg:h-[95vh] sm:h-[100vh] w-[70vw] h-[70vh] bg-white ml-[4vw] lg:ml-[0vw] bg-opacity-[80%] mt-[-40vh] lg:mt-[0vh] relative pt-[5vh] pl-[5vw] pr-[5vw] rounded-[2vw] border-[0.3vw] shadow-inset-register">
                     <div class="p-4 sm:p-7">
                         <div class="text-center">
-                            <h1 class="block text-[3vw] font-bold text-black font-afacad">Sign up</h1>
-                            <p class="mt-2 text-[1vw] text-black font-afacad">
+                            <h1
+                                class="block text-[6vw] lg:mt-[0vh] mt-[-2vh] lg:text-[3vw] font-bold text-black font-afacad">
+                                Sign up</h1>
+                            <p class="mt-2 text-[3vw] lg:text-[1vw] text-black font-afacad">
                                 Already have an account?
                                 <a wire:navigate
-                                    class="text-blue-600 font-afacad text-[1vw] decoration-2 hover:underline font-medium"
+                                    class="text-blue-600 font-afacad text-[3vw] lg:text-[1vw] decoration-2 hover:underline font-medium"
                                     href="/login">
                                     Sign in here
                                 </a>
@@ -22,15 +24,15 @@
 
 
                         <form wire:submit.prevent='save'>
-                            <div class="grid gap-y-4">
+                            <div class="grid gap-y-4 lg:mt-[0vh] mt-[2vh]">
                                 <!-- Form Group -->
 
                                 <div>
                                     <label for="name"
-                                        class="text-[1.8vw] mt-[5vh] text-black font-afacad font-bold">Name</label>
+                                        class="text-[4vw] lg:text-[1.8vw] mt-[5vh] text-black font-afacad font-bold">Name</label>
                                     <div class="relative">
                                         <input type="text" id="name" wire:model="name"
-                                            class="py-[0.5vh] mt-[1vh] px-2 block w-full font-afacad border-black border-[0.2vw] rounded-lg text-[1vw]"
+                                            class="py-[0.5vh] mt-[1vh] px-2 block w-full font-afacad border-black border-[0.2vw] rounded-lg text-[3vw] lg:text-[1vw]"
                                             aria-describedby="name-error">
                                         @error('name')
                                             <div
@@ -50,11 +52,11 @@
 
                                 <div>
                                     <label for="email"
-                                        class="text-[1.8vw] mt-[5vh] text-black font-afacad font-bold">Email
+                                        class="text-[4vw] lg:text-[1.8vw] mt-[5vh] text-black font-afacad font-bold">Email
                                         address</label>
                                     <div class="relative">
                                         <input type="email" id="email" wire:model="email"
-                                            class="py-[0.5vh] mt-[1vh] px-2 block w-full font-afacad border-black border-[0.2vw] rounded-lg text-[1vw]"
+                                            class="py-[0.5vh] mt-[1vh] px-2 block w-full font-afacad border-black border-[0.2vw] rounded-lg text-[3vw] lg:text-[1vw]"
                                             aria-describedby="email-error">
                                         @error('email')
                                             <div
@@ -77,12 +79,13 @@
                                 <div>
                                     <div class="flex justify-between items-center">
                                         <label for="phone_number"
-                                            class="text-[1.8vw] text-black font-afacad font-bold">Phone Number</label>
+                                            class="text-[4vw] lg:text-[1.8vw] text-black font-afacad font-bold">Phone
+                                            Number</label>
 
                                     </div>
                                     <div class="relative">
                                         <input type="text" id="phone_number" wire:model="phone_number"
-                                            class="py-[0.5vh] mt-[1vh] px-2 block w-full font-afacad border-black border-[0.2vw] rounded-lg text-[1vw]"
+                                            class="py-[0.5vh] mt-[1vh] px-2 block w-full font-afacad border-black border-[0.2vw] rounded-lg text-[3vw] lg:text-[1vw]"
                                             aria-describedby="phone_number-error">
                                         @error('phone_number')
                                             <div
@@ -105,12 +108,12 @@
                                 <div>
                                     <div class="flex justify-between items-center">
                                         <label for="password"
-                                            class="text-[1.8vw] text-black font-afacad font-bold">Password</label>
+                                            class="text-[4vw] lg:text-[1.8vw] text-black font-afacad font-bold">Password</label>
 
                                     </div>
                                     <div class="relative">
                                         <input type="password" id="password" wire:model="password"
-                                            class="py-[0.5vh] mt-[1vh] px-2 block w-full font-afacad border-black border-[0.2vw] rounded-lg text-[1vw]"
+                                            class="py-[0.5vh] mt-[1vh] px-2 block w-full font-afacad border-black border-[0.2vw] rounded-lg text-[3vw] lg:text-[1vw]"
                                             aria-describedby="password-error">
                                         @error('password')
                                             <div
@@ -131,7 +134,7 @@
 
 
                                 <button type="submit"
-                                    class="ml-[3vw] relative mt-[4vh] text-[1.5vw] font-bold text-white font-afacad h-[6vh] w-[20vw] rounded-[2.1vw] hover:brightness-[60%] bg-[radial-gradient(223.16%67.62%_at_50%_50%,#698531_26.24%,_#90B042_100%)] disabled:opacity-50 disabled:pointer-events-none">Sign
+                                    class="lg:ml-[3vw] sm:ml-[15vw] sm:mt-[1vh] relative mt-[4vh] lg:text-[1.5vw] font-bold text-white font-afacad h-[4vh] w-[25vw] ml-[27vw] text-[4vw] lg:h-[6vh] lg:w-[20vw] rounded-[2.1vw] hover:brightness-[60%] bg-[radial-gradient(223.16%_67.62%_at_50%_50%,_#698531_26.24%,_#90B042_100%)] disabled:opacity-50 disabled:pointer-events-none">Sign
                                     up</button>
                             </div>
                         </form>
