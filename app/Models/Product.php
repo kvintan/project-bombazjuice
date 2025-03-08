88ea9ee12026859875d61832fdd7c3ef7d323ffd
Kevin Tan <kvintan26@gmail.com>
2025-03-08 21:38:32 +0700

    Add Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use App\Models\OrderItem;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stock'
+    ];
+
+    protected $casts = [
+        'images' => 'array',
+        'description' => 'string',
+    ];
+
+    public function orderItems() {
+        return $this->hasMany(OrderItem::class);
+    }
     //
 }
