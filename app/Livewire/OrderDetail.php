1f24f6b6ebe79b44527c0ebb503322860fba85b0
Kevin Tan <kvintan26@gmail.com>
2025-03-08 19:49:01 +0700

    Update Backend for Order Detail Page

--- app/Livewire/OrderDetail.php
+++ app/Livewire/OrderDetail.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
 use Livewire\Component;
+use App\Models\OrderItem;
+use Livewire\Attributes\Title;
 
+#[Title('Order Detail - BomBazJuice')]
 class OrderDetail extends Component
 {
+    public $order_id;
+
+    public function mount($order_id) {
+        $this->order_id = $order_id;
+    }
+    
     public function render()
     {
-        return view('livewire.order-detail');
+        $order_items = OrderItem::with('product')->where('order_id', $this->order_id)->get();
+        $order = Order::where('id', $this->order_id)->first();
+        return view('livewire.order-detail', [
+            'order_items' => $order_items,
+            'order' => $order
+        ]);
     }
 }
